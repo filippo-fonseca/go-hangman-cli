07ff358ef9e6600e35f597fe857e18077019faba
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-06 16:44:36 -0600

    initial word guess

--- main.go
+++ main.go
@@ -16,10 +16,20 @@ func main() {
 		blanks = append(blanks, "_")
 	}
 
-	fmt.Printf("Word: %s Letter: ", strings.Join(blanks, ""))
+	fmt.Printf("Word: %s Letter: ", strings.Join(blanks, " "))
 
 	var input string
 
 	fmt.Scanln(&input)
 	fmt.Println(input)
+
+	for _, inputLetter := range input {
+		for i, wordLetter := range word {
+			if inputLetter == wordLetter {
+				blanks[i] = string(inputLetter)
+			}
+		}
+	}
+
+	fmt.Println(blanks)
 }
