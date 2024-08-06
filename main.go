832fea8fac9233cb56cfb8a56aa46f10e8d211f5
Filippo Fonseca <filifonsecacagnazzo@gmail.com>
2024-08-06 16:42:25 -0600

    initial user input

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"strings"
 )
 
 func main() {
 	words := []string{"php", "ts", "golang"}
 	word := words[rand.Intn(len(words))]
-	fmt.Println(word)
+
+	blanks := []string{}
+
+	for range word {
+		blanks = append(blanks, "_")
+	}
+
+	fmt.Printf("Word: %s Letter: ", strings.Join(blanks, ""))
+
+	var input string
+
+	fmt.Scanln(&input)
+	fmt.Println(input)
 }
